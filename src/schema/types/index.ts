be4ffdc55e4633cefec68641b44d109746d25f12
Enricco Gemha <enriccogemha@gmail.com>
2025-12-25 21:31:11 -0300

    Add types barrel export

---
+++ src/schema/types/index.ts
@@ -0,0 +1,6 @@
+// Import all types to register them with the builder
+export * from "./game.js";
+export * from "./achievement.js";
+export * from "./user.js";
+export * from "./user-achievement.js";
+export * from "./trophy.js";
